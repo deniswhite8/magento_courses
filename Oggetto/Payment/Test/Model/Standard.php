cb320c5d30e2cc2d2809f7383f43ec097fe987fe
denis belov <deniswhite8@gmail.com>
2014-08-11 22:25:18 +0400

    fix

---
+++ Oggetto/Payment/Test/Model/Standard.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Oggetto Payment extension for Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade
+ * the Oggetto Payment module to newer versions in the future.
+ * If you wish to customize the Oggetto Sales module for your needs
+ * please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category  Oggetto
+ * @package   Oggetto_Payment
+ * @copyright Copyright (C) 2014, Oggetto Web (http://oggettoweb.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Payment model test
+ *
+ * @category   Oggetto
+ * @package    Oggetto_Payment
+ * @subpackage Model
+ * @author     Denis Belov <dbelov@oggettoweb.com>
+ */
+class Oggetto_Payment_Test_Model_Standard extends EcomDev_PHPUnit_Test_Case
+{
+
+    /**
+     * Order place redirect url test
+     *
+     * @return void
+     */
+    public function testOggettoPaymentStandard()
+    {
+        $standard = Mage::getModel('oggetto_payment/standard');
+
+        $this->assertEquals(Mage::getUrl('oggetto_payment/payment/redirect', array('_secure' => true)),
+            $standard->getOrderPlaceRedirectUrl());
+    }
+}
